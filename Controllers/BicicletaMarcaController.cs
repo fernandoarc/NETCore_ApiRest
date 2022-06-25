826fca43271474306c4af3c83357a6c150e6586d
Fernando Raihuanque <fernando.arc02@gmail.com>
2022-06-25 14:24:32 -0400

    controlador  modelo - se crean metodos post y put para entidad marca

--- Controllers/BicicletaMarcaController.cs
+++ Controllers/BicicletaMarcaController.cs
@@ -44,12 +44,59 @@ namespace NetCore_API.Controllers
         /// <returns>Objeto BicicletaMarca</returns>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BicicletaMarca))]
-        public async Task<ActionResult> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
             var ListMarcas = await _context.BicicletaMarca.FindAsync(id);
             return  Ok(ListMarcas);
         }
 
+        /// <summary>
+        /// Metodo encargado de registrar una nueva marca en sistema
+        /// </summary>
+        /// <param name="bicicletaMarca">Objeto Bicicle Marca a registrar en sistema</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Add([FromBody] BicicletaMarca bicicletaMarca)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.BicicletaMarca.Add(bicicletaMarca);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
 
+        /// <summary>
+        /// Metodo encargado de actualizar una marca ya existente
+        /// </summary>
+        /// <param name="bicicletaMarca">Objeto marca a actualizar</param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Edit([FromBody]BicicletaMarca bicicletaMarca)
+        {
+            var bicicletaMarcaSql = await _context.BicicletaMarca.FindAsync(bicicletaMarca.IdBicicletaMarca);
+            if (bicicletaMarcaSql == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    bicicletaMarcaSql.Nombre = bicicletaMarca.Nombre;
+                    _context.Update(bicicletaMarcaSql);
+                    await _context.SaveChangesAsync();
+                }
+                catch (System.Exception ex)
+                {
+                    
+                    return BadRequest(ex.Message);
+                }
+            }
+            return Ok();
+        }
     }
 }
--- Model/Entidades/Bike/BicicletaMarca.cs
+++ Model/Entidades/Bike/BicicletaMarca.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace NetCore_API.Model.Entidades.Bike
 {
@@ -8,6 +9,6 @@ namespace NetCore_API.Model.Entidades.Bike
         [Key]
         public int IdBicicletaMarca { get; set; }
         public string Nombre { get; set; }
-        public List<BicicletaModelo> BicicletaModelo { get; set; }
+        protected internal List<BicicletaModelo> BicicletaModelo { get; set; }
     }
 }
